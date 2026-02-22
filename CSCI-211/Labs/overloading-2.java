38edf1768b53c18fc7a43dacd4a0f73732ff169a
Ali Aldahmani <144395155+Ali-Aldahmani@users.noreply.github.com>
2026-02-22 17:14:53 +0400

    Add files via upload

---
+++ CSCI-211/Labs/overloading-2.java
@@ -0,0 +1,24 @@
+public class overloading {
+    public static void main(String[] args) {
+        /* 
+        TODO: Create two methods with the same name: convertToCentimeters.
+         1. The first method has one parameter of type int, which represents the entire height in inches. You'll convert inches to centimeters,
+          in this method, and pass back the number of centimeters, as a double.
+         2. The second method has two parameters of type int, one to represent height in feet, and one to represent the remaining height in inches.
+          So if a person is 5 foot, 8 inches, the values 5 for feet and 8 for inches would be passed to this method. This method will convert feet and inches to just inches,
+           then call the first method, to get the number of centimeters, also returning the value as a double.
+        */
+       System.out.println("5ft, 8in = " + convertToCentimeters(5, 8));
+       System.out.println("68in = " + convertToCentimeters(68) + " cm");
+    }
+    public static double convertToCentimeters(int inches) {
+        return inches * 2.54;
+    }
+    public static double convertToCentimeters(int feet, int inches) {
+        // ? return convertToCentimeters((feet * 12) + inches)
+        int feetToInches = feet * 12;
+        int totalInches = feetToInches + inches;
+        double result = convertToCentimeters(totalInches);
+        return  result;
+    }
+}
